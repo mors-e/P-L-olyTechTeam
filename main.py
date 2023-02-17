02e20cded4d6fa7d4b3a815f27f654f3d252a4fd
mors-e <mors@nemors.ru>
2023-02-17 20:06:19 +0300

    bot

---
+++ main.py
@@ -0,0 +1,43 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+
+from config.config import Config, load_config
+
+logger = logging.getLogger(__name__)
+storage: RedisStorage2 = RedisStorage2()
+
+
+def register_all_handlers(dp: Dispatcher) -> None:
+    pass
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
+               u'[%(asctime)s] - %(name)s - %(message)s')
+
+    logger.info("Starting Bot")
+
+    config: Config = load_config('.env')
+
+    bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp: Dispatcher = Dispatcher(bot, storage=storage)
+
+    #await set_main_menu(dp)
+    register_all_handlers(dp)
+
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.closed()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error('Bot stopped!')
